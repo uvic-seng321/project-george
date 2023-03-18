f561269583644718fd3bc128421714cd93bc9924
Christian Bookout <christianmbookout@gmail.com>
2023-03-17 18:11:19 -0700

    allow temporary images to be stored for testing

--- the_watering_hole/api/test/conftest.py
+++ the_watering_hole/api/test/conftest.py
@@ -1,25 +1,23 @@
 from flask import Flask
-import testing.mysqld
 import pytest
-from api import app as flask_app
+import api
+import os
 
 @pytest.fixture()
 def app():
-    flask_app.testing = True
-    flask_app.config.from_object('config.DevConfig')
-    yield flask_app
+    yield api.app
 
 @pytest.fixture()
 def client(app : Flask):
     return app.test_client()
 
 @pytest.fixture()
-def db():
-    # TODO yield database
-    return
-
-@pytest.fixture()
 def runner(app : Flask):
     return app.test_cli_runner()
 
+@pytest.fixture(scope="session")
+def temp_dir(tmp_path_factory):
+    dir = str(tmp_path_factory.mktemp("image_files"))
+    os.environ["IMAGE_DIR"] = dir
+    return dir
 # TODO database
--- the_watering_hole/api/test/test_post.py
+++ the_watering_hole/api/test/test_post.py
@@ -1,5 +1,8 @@
+import io
 from flask import Flask
 import pytest
+import os
+from PIL import Image
 
 from constants import *
 
@@ -28,24 +31,32 @@ def upload_url(user : int = 1,
     req += "".join(["&tags=" + tag for tag in tags])
     return req
 
-def test_upload_post(client: Flask, test_image):
+def test_upload_post(client: Flask, test_image, temp_dir):
     '''Test that the uploadPost endpoint successfully uploads a post and returns 200'''
 
     # Create a post with simple data for testing.
-    url = upload_url(lat=91, long=181, tags = ["A", "B"])
-    response = client.post(url, data = test_image)
+    url = upload_url(lat=1, long=1, radius=1, tags = ["A", "B"])
+    response = client.post(url, data=test_image)
 
     # Check that the post was uploaded successfully
-    assert response.status_code == 200
+    assert response.status_code == 200, "uploadPost should return 200 signifying its success"
+
+    assert len(os.listdir(temp_dir)) == 1, "There should only be one file uploaded"
+
+    # Ensure the uploaded image is the same image as the one POSTed
+    image_url = os.listdir(temp_dir)[0]
+    image = Image.open(temp_dir + "/" + image_url)
+    assert image == Image.open(io.BytesIO(test_image)), "The uploaded image in the file system should be the same as the test image POSTed"
 
 def test_invalid_location(client: Flask, test_image):
     '''Test that the getPosts endpoint returns an error when given an invalid location'''
     # Create a post with an invalid location
-    url = upload_url(lat=91, long=181, tags = ["A", "B"])
-    response = client.post(url, data = test_image)
-    assert response.text == INVALID_LOCATION and response.status_code == 400
+    url = upload_url(lat=91, long=181, radius=1, tags = ["A", "B"])
+    response = client.post(url, data=test_image)
+    assert response.text == INVALID_LOCATION and response.status_code == 400, "uploadPost should return 400 when given an invalid location"
 
-def test_no_radius(client: Flask):
+def test_no_radius(client: Flask, test_image):
     '''Test that the getPosts endpoint returns an error when given a location but no radius'''
-    response = client.post()
-    assert response.text == NO_RADIUS_GIVEN and response.status_code == 400
+    url = upload_url(lat=1, long=1, tags = ["A", "B"])
+    response = client.post(url, data=test_image)
+    assert response.text == NO_RADIUS_GIVEN and response.status_code == 400, "uploadPost should return 400 when given a location but no radius"

88ba771297ea2d4667b1f0c003445a15d14f7efb
Christian Bookout <christianmbookout@gmail.com>
2023-03-29 15:01:16 -0700

    create utils for interfacing with the database

---
+++ the_watering_hole/api/utils.py
@@ -0,0 +1,27 @@
+from db import get_db
+
+db = get_db()
+
+def send_query(query, args):
+    db.reconnect()
+    cur = db.cursor()
+    cur.execute(query, args)
+    result = cur.fetchall()
+    db.commit()
+    return result
+
+def send_upload_post(args):
+    db.reconnect()
+    cur = db.cursor()
+    cur.callproc("george.uploadPost", args)
+    result = cur.fetchall()
+    db.commit()
+    return result
+
+def send_get_posts(args):
+    db.reconnect()
+    cur = db.cursor()
+    cur.callproc("george.getPosts", args)
+    result = [r.fetchall() for r in cur.stored_results()]
+    db.commit()
+    return result[0]

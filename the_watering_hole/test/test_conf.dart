d3d8906302059910999220b0dad7efb4365604e3
Christian Bookout <christianmbookout@gmail.com>
2023-04-04 17:16:26 -0700

    clean up test config

--- the_watering_hole/test/test_conf.dart
+++ the_watering_hole/test/test_conf.dart
@@ -1,13 +1,12 @@
 import 'dart:async';
 import 'package:flutter_gherkin/flutter_gherkin.dart';
 import 'package:gherkin/gherkin.dart';
-import 'steps/post_steps.dart';
 
 Future<void> main() {
   final config = FlutterTestConfiguration()
-    ..features = [RegExp('./features/*.feature')]
+    ..features = [RegExp('features/*.feature')]
     ..reporters = [StdoutReporter()]
-    ..stepDefinitions = [UploadPostPageStep()]
+    ..stepDefinitions = []
     ..restartAppBetweenScenarios = true
     ..targetAppPath = "./test.dart";
   return GherkinRunner().execute(config);
